45de671d0339b14d439fdb6d0fa799a49b89676f
TitadachFF <120708254+TitadachFF@users.noreply.github.com>
2023-09-25 10:17:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Restautant from './pages/Restautant';
-import Add from './pages/Add';
-import Search from './pages/Search';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Restaurant from './pages/Restaurant'
+import Add from './pages/Add'
+import Search from './pages/Search'
 import Update from './pages/Update';
 
-
 function App() {
+  
 
   return (
     <BrowserRouter>
-      <NavBar />
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<Restautant /> }/>
-          <Route path="/add" element={<Add /> }/>
-          <Route path="/search" element={<Search /> }/>
-        </Routes>
-      </div>
+    <NavBar/>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Restaurant />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/Search" element={<Search />} />
+        <Route path="/Update/:restaurantId" element={<Update />} />
+      </Routes>
+    </div>
     </BrowserRouter>
   );
 }
