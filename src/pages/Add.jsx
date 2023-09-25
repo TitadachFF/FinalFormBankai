e4d5bc8ee31d3806b7628cdd515be6426136f613
TitadachFF <120708254+TitadachFF@users.noreply.github.com>
2023-09-25 10:18:40 +0700

    Update Add.jsx

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,93 +1,100 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
-const url = import.meta.env.VITE_BASE_URL;
+const URL = import.meta.env.VITE_BASE_URL;
 const USERNAME = import.meta.env.VITE_BASE_USERNAME;
 const PASSWORD = import.meta.env.VITE_BASE_PASSWORD;
-
 const config = {
   auth: {
     username: USERNAME,
     password: PASSWORD,
   },
+ 
+  
 };
 
 const Add = () => {
-  const [restaurant, setRestaurants] = useState({
-    name: "",
-    type: "",
-    image: ""
+  const [restaurant, setRestaurant] =useState({
+    name:"",
+    type:"",
+    image:""
   })
   const navigate = useNavigate();
-  const [error, setError] = useState(false);
+  const [error , setError] = useState(false);
 
-  const handleChage = (e) => {
-    setRestaurants((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  const handleChange = (e) => {
+    setRestaurant((prev)=>({...prev,[e.target.name]:e.target.value}));
+
   }
 
-  const handleClick = async (e) => {
-    e.prevenDefault();
+  const handleClick = async (e) =>{
+    e.preventDefault();
     try {
-      await axios.post(`${URL}/restaurants`, restaurant, config);
-      navigate("/")
+      await axios.post(`${URL}/restaurants`,restaurant,config);
+      navigate('/');
     } catch (error) {
       console.error(error);
-      setError(true)
+      setError(true);
     }
   }
-  const handleClear = (e) => {
-    setRestaurants({
-      name: "",
-      type: "",
-      image: "",
-    })
-    setError(false);
-  }
+
+
   return (
     <div className="container">
       <h1>Grab Restaurant</h1>
       <div className="row form">
         <div className="col-6 card justify-content-center">
-          <h5 className="card-header">Add new restaurant</h5>
-          <div className="card-body"></div>
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Restaurant name</label>
-              <input
+          <h5 className='card-header'>Add new restaurant</h5>
+          <div className="error">{error && "somethingwrong"}</div>
+          <div className="card-body">
+
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Restaurant name</label>
+                <input 
                 type="text"
-                className="form-control"
-                name="name"
-                placeholder="Restaurant name"
-                onChange={handleChage}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Restaurant type</label>
-              <input
-                type="text"
-                className="form-control"
-                name="type"
-                placeholder="Restaurant type"
-                onChange={handleChage} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Restaurant imageurl</label>
-              <input
-                type="text"
-                className="form-control"
-                name="image"
-                placeholder="Restaurant image"
-                onChange={handleChage} />
-            </div>
-            <br />
-            <Link to="" className="btn btn-success" onClick={handleClick}>
-              Add
-            </Link>
-            <Link to="" className="btn btn-danger" onClick={handleClear}>
-              Clear
-            </Link>
-          </form>
+                className='form-control' 
+                name="name" 
+                placeholder='Restaurant name'
+                onChange={handleChange}
+                value={restaurant.name} />
+              </div>
+
+              
+                <div className="form-group">
+                  <label htmlFor="type">Restaurant Type</label>
+                  <input
+                    type="text"
+                    className='form-control'
+                    name="type"
+                    placeholder='Restaurant type' 
+                    onChange={handleChange}
+                    value={restaurant.type}/>
+                </div>
+
+                
+                  <div className="form-group">
+                    <label htmlFor="image">Restaurant image</label>
+                    <input
+                      type="text"
+                      className='form-control'
+                      name="image"
+                      placeholder='Restaurant image'
+                      onChange={handleChange} 
+                      value={restaurant.image}/>
+                  </div>
+
+                  <Link to="" className='btn btn-success' onClick={handleClick}>
+                    Add
+                  </Link>
+              <Link to="/" className='btn btn-danger' >
+                    Cancel
+                  </Link>
+
+
+            </form>
+          </div>
         </div>
       </div>
     </div>
