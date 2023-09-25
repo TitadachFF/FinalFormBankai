e75dd012d4d8bf74f100a4a59e8dc6d791932899
TitadachFF <120708254+TitadachFF@users.noreply.github.com>
2023-09-25 10:19:25 +0700

    Update Restautant.jsx

--- src/pages/Restautant.jsx
+++ src/pages/Restautant.jsx
@@ -1,48 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from "axios";
 import Card from '../components/Card';
 
-const url = import.meta.env.VITE_BASE_URL;
+const URL = import.meta.env.VITE_BASE_URL;
 const USERNAME = import.meta.env.VITE_BASE_USERNAME;
 const PASSWORD = import.meta.env.VITE_BASE_PASSWORD;
-
 const config = {
     auth: {
-        username: USERNAME,
-        password: PASSWORD,
+        username:USERNAME,
+        password:PASSWORD,
     },
 };
 
 const Restaurant = () => {
     const [restaurants, setRestaurants] = useState([]);
-
-    useEffect(() => {
-        const fetchAllRestaurant = async () => {
+    useEffect(()=>{
+        const fetcAllRestaurant = async () =>{
             try {
-                const res = await axios.get(`${url}/restaurants`, config);
+                const res = await axios.get(`${URL}/restaurants`, config)
                 setRestaurants(res.data);
             } catch (error) {
                 console.error(error);
             }
-        };
-        fetchAllRestaurant();
-    }, []);
+        }
+        fetcAllRestaurant();
+    },[])
 
-    return (
-        <div>
-            <h1>Restaurant</h1>
-            <div className='row'>
-                <div className='restaurants'>
-                    {restaurants.map(restaurant => {
-                        return (
-                            <Card restaurant={restaurant} key={restaurant.id} />
+    const handDelete = async (id) =>{
+        try {
+            await axios.delete(`${URL}/restaurants/${id}`, config);
+            window.location.reload();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+  return (
+    <div>
+        <h1>Restaurant</h1>
+        <div className="row">
+            <div className="restaurants">
+                {
+                    restaurants.map(restaurant =>{
+                        return(
+                            <Card restaurant={restaurant} 
+                            key={restaurant.id}/>
                         );
-                    })}
-                </div>
+                    })
+                }
             </div>
         </div>
-    );
-};
+    </div>
+  )
+}
 
-export default Restaurant;
+export default Restaurant
