198d5803b17888f49180dacedbb0646d7ad3bf5b
TitadachFF <titadach15632441@gmail.com>
2023-11-01 16:28:27 +0700

    Add Loading

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Card from "../components/Card";
 import authHeader from "../services/auth.header";
+import Loading from "../components/Loading";
+import * as loadingData from "../loading/restaurant.json";
 
 const URL = import.meta.env.VITE_BASE_URL;
 const USERNAME = import.meta.env.VITE_BASE_USERNAME;
@@ -17,15 +19,18 @@ const config = {
 
 const Restaurant = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchAllRestaurant = async () => {
       try {
         const res = await axios.get(`${URL}/restaurants`, config);
         setRestaurants(res.data);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
     };
+    setLoading(true);
     fetchAllRestaurant();
   }, []);
 
@@ -42,17 +47,21 @@ const Restaurant = () => {
     <div>
       <h1>Restaurant</h1>
       <div className="row">
-        <div className="restaurants">
-          {restaurants.map((restaurant) => {
-            return (
-              <Card
-                restaurant={restaurant}
-                key={restaurant.id}
-                handelDelete={handDelete}
-              />
-            );
-          })}
-        </div>
+        {!loading ? (
+          <div className="restaurants">
+            {restaurants.map((restaurant) => {
+              return (
+                <Card
+                  restaurant={restaurant}
+                  key={restaurant.id}
+                  handelDelete={handDelete}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <Loading animation={loadingData} />
+        )}
       </div>
     </div>
   );
